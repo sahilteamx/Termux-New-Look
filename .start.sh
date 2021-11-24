7ae3e3c6f832c071ca6aa298821d9096e6171012
Sahil Siddiqui <86236030+sahilteamx@users.noreply.github.com>
2021-11-24 07:39:54 +0530

    Create .start.sh

---
+++ .start.sh
@@ -0,0 +1,13 @@
+clear
+printf "===========================================\n"
+printf "		\033[96m Termux-New-Look\033[0m\n"
+printf "===========================================\n\n"
+printf "\033[96m ● Feature :- \n"
+printf "\033[1;92m"
+printf "
+ • Added menu command to see menu
+ • Added many shortcut
+ • Fixed some problems
+printf "===========================================\n"
+printf "		\033[93m Termux-New-Look\033[0m\n"
+printf "===========================================\n\n"
