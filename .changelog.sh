d5a70c0bb32600737f8c61249acd4ae5f2c705a4
Sahil Siddiqui <86236030+sahilteamx@users.noreply.github.com>
2021-11-24 07:38:40 +0530

    Create .changelog.sh

---
+++ .changelog.sh
@@ -0,0 +1,50 @@
+
+a="$(tput setaf 10)"
+b="$(tput setaf 11)"
+c="$(tput setaf 77)"
+d="$(tput setaf 13)"
+e="$(tput setaf 14)"
+f="$(tput setaf 75)"
+g="$(tput setaf 68)"
+h="$(tput setaf 9)"
+i="$(tput setaf 32)"
+j="$(tput setaf 33)"
+k="$(tput setaf 41)"
+l="$(tput setaf 40)"
+m="$(tput setaf 42)"
+n="$(tput setaf 45)"
+o="$(tput setaf 46)"
+toilet -f term -F gay =============================================
+echo "$a Termux-New-Look $b Create $c by $d Sahil"
+toilet -f term -F gay =============================================
+echo
+echo
+echo "$a Many Feature Available Termux-New-Look"
+echo "$a Termux-New-Look specially for new Termux user"
+echo
+echo "$a Termux-New-Look Last Updated$b  2 january 2020"
+echo 
+echo "$a My github link$d :-$e http://github.com/sahilteamx"
+echo
+printf "$a This Tool Github Link$f \n\n$e http://github.com/sahilteamx/Termux-New-Look\n\n"
+echo -e "\033[92m 		Termux-New-Look Changelog:-"
+printf "\033[93m\n Termux-New-Look 4.5\n"
+echo -e "\033[96m"
+printf " [+] Added auto download ngrok\n"
+#printf " [+] Added TI script in menu\n"
+#printf " [+] Added Shortcut commands to install & open tools\n"
+printf " [+] Some problem fix\n"
+echo
+
+toilet -f term -F gay =============================================
+echo "	$a Termux-New-Look $b About $c End"
+toilet -f term -F gay =============================================
+echo
+echo
+
+
+
+
+
+
+
